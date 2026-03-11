2f5b96870f699d6f2ec56a15710064e00772848a
msandonini <mirco.ms16@gmail.com>
2026-03-11 11:11:47 +0100

    vault backup: 2026-03-11 11:11:47

---
+++ Multimedia Data Processing/Lab/20260311 assignment/01 frequencies/main.cpp
@@ -0,0 +1,47 @@
+#include <fstream>
+#include <map>
+#include <ostream>
+#include <print>
+
+int main(int argc, char const* argv[]) {
+	if (argc != 3) {
+		std::println("3 arguments needed. Usage:");
+		std::println("\tfrequencies <input_file> <output_file>");
+		return 1;
+	}
+
+	std::ifstream is(argv[1], std::ios::binary);
+
+	if (!is) {
+		std::println("Error: unable to open file \"%s\"", argv[1]);
+		return 1;
+	}
+
+	std::map<unsigned char, unsigned int> m;
+
+	unsigned char d;
+	while (is >> d) {
+		if (m.contains(d)) {
+			m[d]++;
+		} else {
+			m[d] = 1;
+		}
+	}
+
+	is.close();
+
+	std::ofstream os(argv[2]);
+
+	if (!os) {
+		std::println("Error: Output stream not open");
+		return 1;
+	}
+
+	for (const auto& pair : m) {
+		unsigned char k = pair.first;
+		unsigned int v = pair.second;
+		std::println(os, "{:02x}\t{}", k, v);
+	}
+
+	return 0;
+}
