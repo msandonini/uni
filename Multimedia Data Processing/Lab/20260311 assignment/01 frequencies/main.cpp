90982d11c13d281da325df4668e4d87319492831
msandonini <mirco.ms16@gmail.com>
2026-03-11 11:22:54 +0100

    vault backup: 2026-03-11 11:22:54

--- Multimedia Data Processing/Lab/20260311 assignment/01 frequencies/main.cpp
+++ Multimedia Data Processing/Lab/20260311 assignment/01 frequencies/main.cpp
@@ -19,12 +19,25 @@ int main(int argc, char const* argv[]) {
 
 	std::map<unsigned char, unsigned int> m;
 
+	/*
 	unsigned char d;
-	while (is >> d) {
+	while (is >> std::noskipws >> d) {
 		if (m.contains(d)) {
 			m[d]++;
 		} else {
 			m[d] = 1;
+		}
+	}
+	*/
+
+	char d;
+	while (is.get(d)) {
+		unsigned char c = d;
+
+		if (m.contains(c)) {
+			m[c]++;
+		} else {
+			m[c] = 1;
 		}
 	}
 
