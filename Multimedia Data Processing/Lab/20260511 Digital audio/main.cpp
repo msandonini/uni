91909fdc1132726769d2f05c593f42702e94c8e8
msandonini <mirco.ms16@gmail.com>
2026-05-11 12:27:59 +0200

    vault backup: 2026-05-11 12:27:59

---
+++ Multimedia Data Processing/Lab/20260511 Digital audio/main.cpp
@@ -0,0 +1,29 @@
+#include <cmath>
+#include <cstdint>
+#include <fstream>
+#include <numbers>
+#include <vector>
+
+int main(void) {
+	int sps = 44100;  // Sampling rate [samples/s]
+	std::vector<uint16_t> samples;
+	int duration = 2;	// [s]
+	int A = INT16_MAX;	// Amp
+	double f = 440;		// [Hz] -> 440 Hz corresponds to the A chord
+
+	for (size_t i = 0; i < sps * duration; i++) {
+		double t = static_cast<double>(i) / sps;
+		double val = A * sin(2 * std::numbers::pi * f * t);
+
+		int16_t sample = static_cast<int16_t>(lround(val));
+
+		samples.push_back(sample);
+	}
+
+	std::ofstream os("output.raw", std::ios::binary);
+	if (!os) {
+		return 1;
+	}
+
+		return 0;
+}
